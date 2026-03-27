053567a1445b06fb70cca7cf37882abeba79454a
Tquang <n.tien.quang1210@gmail.com>
2026-03-27 09:52:13 +0700

    update UserFactory

--- src/database/factories/UserFactory.php
+++ src/database/factories/UserFactory.php
@@ -27,7 +27,7 @@ class UserFactory extends Factory
             'email' => $this->faker->unique()->safeEmail,
             'email_verified_at' => now(),
             'password' => '$2y$10$92IXUNpkjO0rOQ5byMi.Ye4oKoEa3Ro9llC/.og/at2.uheWG/igi',
-            'role' => $this->faker->randomElement(['admin', 'user']),
+            'role' => "user",
             'remember_token' => Str::random(10),
         ];
     }
